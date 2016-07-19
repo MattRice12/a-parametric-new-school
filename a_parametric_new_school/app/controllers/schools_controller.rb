adec0be9c4eb2aac2938368c7f350842e1951515
Matt Rice <mattrice12@outlook.com>
2016-07-19 14:22:36 -0500

    Got routes and controllers to communicate with views too Yay

--- a_parametric_new_school/app/controllers/schools_controller.rb
+++ a_parametric_new_school/app/controllers/schools_controller.rb
@@ -1,2 +1,26 @@
 class SchoolsController < ApplicationController
+  def index
+    render template: 'schools/index.html.erb', locals: {
+      schools: School.all
+    }
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
 end
--- a_parametric_new_school/app/controllers/students_controller.rb
+++ a_parametric_new_school/app/controllers/students_controller.rb
@@ -1,2 +1,25 @@
 class StudentsController < ApplicationController
+  def index
+    render template: 'students/index.html.erb', locals: {
+      students: Student.all
+    }
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
 end
