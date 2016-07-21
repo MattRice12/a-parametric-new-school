b7c822499f9447f803acebf2b7fb3c7f3acaa355
Matt Rice <mattrice12@outlook.com>
2016-07-20 22:02:47 -0500

    samesies

--- a_parametric_new_school/app/controllers/sessions_controller.rb
+++ a_parametric_new_school/app/controllers/sessions_controller.rb
@@ -1,5 +1,5 @@
 class SessionsController < ApplicationController
-  skip_before_action :authenticate_user!
+  skip_before_action :authenticate_user!, except: [:destroy, :sign_out]
 
   def sign_in
   end
